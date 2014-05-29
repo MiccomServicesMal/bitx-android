3e29a56099100e0dc70550f668386869adc290e6
Alex Koller <alex@22seven.com>
2014-05-29 20:23:54 +0200

    Improved logging util.

--- bitx-android/src/main/java/com/twentytwoseven/android/bitx/util/LogUtil.java
+++ bitx-android/src/main/java/com/twentytwoseven/android/bitx/util/LogUtil.java
@@ -3,6 +3,9 @@ package com.twentytwoseven.android.bitx.util;
 import android.util.Log;
 import com.twentytwoseven.android.bitx.BuildConfig;
 
+import java.util.MissingFormatArgumentException;
+import java.util.UnknownFormatConversionException;
+
 public class LogUtil {
 
     private static final boolean INFO_ENABLED = BuildConfig.DEBUG;
@@ -12,32 +15,32 @@ public class LogUtil {
     private LogUtil() {}
 
     public static void v(String tag, String message, Object... args) {
-        if (BuildConfig.DEBUG && Log.isLoggable(tag, Log.VERBOSE)) {
-            Log.v(tag, String.format(message, args));
+        if (BuildConfig.DEBUG || Log.isLoggable(tag, Log.VERBOSE)) {
+            Log.v(tag, safeFormat(message, args));
         }
     }
 
     public static void v(String tag, String message, Throwable cause) {
-        if (BuildConfig.DEBUG && Log.isLoggable(tag, Log.VERBOSE)) {
+        if (BuildConfig.DEBUG || Log.isLoggable(tag, Log.VERBOSE)) {
             Log.v(tag, message, cause);
         }
     }
 
     public static void d(String tag, String message, Object... args) {
-        if (BuildConfig.DEBUG && Log.isLoggable(tag, Log.DEBUG)) {
-            Log.d(tag, String.format(message, args));
+        if (BuildConfig.DEBUG || Log.isLoggable(tag, Log.DEBUG)) {
+            Log.d(tag, safeFormat(message, args));
         }
     }
 
     public static void d(String tag, String message, Throwable cause) {
-        if (BuildConfig.DEBUG && Log.isLoggable(tag, Log.DEBUG)) {
+        if (BuildConfig.DEBUG || Log.isLoggable(tag, Log.DEBUG)) {
             Log.d(tag, message, cause);
         }
     }
 
     public static void i(String tag, String message, Object... args) {
         if (INFO_ENABLED) {
-            Log.i(tag, String.format(message, args));
+            Log.i(tag, safeFormat(message, args));
         }
     }
 
@@ -49,7 +52,7 @@ public class LogUtil {
 
     public static void w(String tag, String message, Object... args) {
         if (WARN_ENABLED) {
-            Log.w(tag, String.format(message, args));
+            Log.w(tag, safeFormat(message, args));
         }
     }
 
@@ -61,13 +64,23 @@ public class LogUtil {
 
     public static void e(String tag, String message, Object... args) {
         if (ERRO_ENABLED) {
-            Log.e(tag, String.format(message, args));
+            Log.e(tag, safeFormat(message, args));
         }
     }
 
     public static void e(String tag, String message, Throwable cause) {
         if (ERRO_ENABLED) {
             Log.e(tag, message, cause);
+        }
+    }
+    private static String safeFormat(String message, Object... args) {
+        try {
+            return String.format(message, args);
+        } catch (UnknownFormatConversionException | MissingFormatArgumentException | NullPointerException e) {
+            for (Object a : args) {
+                message += ", " + a;
+            }
+            return message;
         }
     }
 }
