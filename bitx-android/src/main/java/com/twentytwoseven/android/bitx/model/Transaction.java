fc54de96b43760cacbd6a5516d8893927362188f
Alex Koller <alex@22seven.com>
2014-05-29 18:10:10 +0200

    Fix for transactions model based on latest API changes.

--- bitx-android/src/main/java/com/twentytwoseven/android/bitx/model/Transaction.java
+++ bitx-android/src/main/java/com/twentytwoseven/android/bitx/model/Transaction.java
@@ -1,26 +1,11 @@
 package com.twentytwoseven.android.bitx.model;
 
-import com.google.gson.annotations.SerializedName;
-
 public class Transaction {
-    @SerializedName("Txid")
-    public String id;
-
-    @SerializedName("Timestamp")
+    public String txid;
     public long timestamp;
-
-    @SerializedName("Type")
     public String type;
-
-    @SerializedName("Address")
     public String address;
-
-    @SerializedName("Description")
     public String description;
-
-    @SerializedName("Amount")
     public double amount;
-
-    @SerializedName("Pending")
     public boolean pending;
 }
--- bitx-android/src/main/java/com/twentytwoseven/android/bitx/model/TransactionList.java
+++ bitx-android/src/main/java/com/twentytwoseven/android/bitx/model/TransactionList.java
@@ -1,13 +1,8 @@
 package com.twentytwoseven.android.bitx.model;
 
-import com.google.gson.annotations.SerializedName;
-
 import java.util.List;
 
 public class TransactionList {
-    @SerializedName("TotalCount")
     public int totalCount;
-
-    @SerializedName("Transactions")
     public List<Transaction> transactions;
 }
